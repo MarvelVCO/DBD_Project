725b6106bdcb35516849f532d1917c3a72b534d4
unknown <BT_4N2_05@BKTECH.LOCAL>
2025-03-24 13:38:32 -0400

    Added Names File

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,13 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+    }
+
+    public static void students() {
+
     }
 
     public static ArrayList<String> getFileData(String fileName) {
