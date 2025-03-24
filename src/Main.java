544d1b11a69b456ff31ac36daa9766559db18bdd
DaSmSchool <145377174+DaSmSchool@users.noreply.github.com>
2025-03-24 13:48:28 -0400

    remove dob from table creation

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,10 @@ public class Main {
     }
 
     public static void create_database() {
-        System.out.println("sqlite3 db \"CREATE TABLE Students ( first_name varchar(255), last_name varchar(255), DOB date, student_id integer Primary Key );\"");
+        System.out.println("sqlite3 db \"CREATE TABLE Students ( first_name varchar(255), last_name varchar(255), student_id integer Primary Key );\"");
         System.out.println("sqlite3 db \"CREATE TABLE Courses ( course_name varchar(255), type_id integer, course_id integer PRIMARY KEY, FOREIGN KEY (type_id) REFERENCES Course_types(type_id) );\"");
         System.out.println("sqlite3 db \"CREATE TABLE Course_types ( type_name varchar(255), type_id integer PRIMARY KEY );\"");
-        System.out.println("sqlite3 db \"CREATE TABLE Teachers ( first_name varchar(255), last_name varchar(255), teacher_id integer PRIMARY KEY, dob date, department_id integer, FOREIGN KEY (department_id) REFERENCES departments(department_id) );\"");
+        System.out.println("sqlite3 db \"CREATE TABLE Teachers ( first_name varchar(255), last_name varchar(255), teacher_id integer PRIMARY KEY, department_id integer, FOREIGN KEY (department_id) REFERENCES departments(department_id) );\"");
         System.out.println("sqlite3 db \"CREATE Table Classes ( course_id integer, class_time date, teacher_id integer, classroom varchar(255), class_id integer PRIMARY KEY, FOREIGN KEY (course_id) references Courses(course_id), FOREIGN KEY (teacher_id) references Teachers(teacher_id) );\"");
         System.out.println("sqlite3 db \"CREATE TABLE Grades (assignment_id integer, student_id integer, grade float, FOREIGN KEY ( assignment_id ) REFERENCES Assignments( assignment_id ), FOREIGN KEY ( student_id) REFERENCES Students( student_id) );\"");
         System.out.println("sqlite3 db \"CREATE TABLE Assignment_types ( type_id integer PRIMARY KEY, type_name varchar(255) );\"");
@@ -23,7 +23,7 @@ public class Main {
     }
 
     public static void students() {
-        
+
     }
 
     public static ArrayList<String> getFileData(String fileName) {
