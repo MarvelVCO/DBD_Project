5d5a82880abf2d63e1b090dabbcf57837b87affb
unknown <BT_4N2_05@BKTECH.LOCAL>
2025-03-24 13:59:46 -0400

    created student genorator

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        students();
     }
 
     public static void students() {
-
+        ArrayList<String> students = getFileData("src/names.txt");
+        for (int i = 0; i < 500; i++) {
+            String first_name = students.get((int) (Math.random() * students.size()));
+            String last_name = students.get((int) (Math.random() * students.size()));
+            first_name = first_name.substring(0, 1).toUpperCase() + first_name.substring(1);
+            last_name = last_name.substring(0, 1).toUpperCase() + last_name.substring(1);
+            System.out.println("sqlite3 DB \"INSERT INTO Students ( first_name, last_name ) VALUES ( '" + first_name + "', '" + last_name + "' )\";");
+        }
     }
 
     public static ArrayList<String> getFileData(String fileName) {
