6160f5ab40b434e7b45c375d6f848fb4cc1efbe0
unknown <BT_4N2_05@BKTECH.LOCAL>
2025-04-09 13:55:43 -0400

    finished courses

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
-import java.security.spec.RSAOtherPrimeInfo;
 import java.util.*;
 
 public class Main {
@@ -12,6 +10,7 @@ public class Main {
     static ArrayList<String> teachers;
     static ArrayList<String> students;
     static ArrayList<String> classrooms;
+    static ArrayList<String> assignments;
 
 
     public static void main(String[] args) {
@@ -23,7 +22,10 @@ public class Main {
         courseTypes = generateCourseTypes();
         generateCourses();
         generateClassrooms(720);
-//        generateClasses();
+        generateClasses();
+        generateStudents(5000);
+        generateAssignments(courses.size() * 15);
+        generateTeachers(teachers.getFirst().split(",").length);
     }
 
     public static void createDatabase() {
@@ -98,12 +100,13 @@ public class Main {
         }
     }
 
-    public static ArrayList<String> generateAssignments(int n) {
+    // TODO: Make it do 12 minor 3 major ty very much
+    public static void generateAssignments(int n) {
         for (int i = 0; i < assignmentTypes.length; i++) {
             System.out.println("INSERT INTO AssignmentTypes ( type_id , type_name ) VALUES (" + (i + 1) + ", '" + assignmentTypes[i] + "');");
         }
 
-        ArrayList<String> assignments = new ArrayList<>();
+        assignments = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
             String assignmentName = "Assignment " + (i + 1);
@@ -111,9 +114,9 @@ public class Main {
             int assignmentTypeInt = (int) (Math.random() * assignmentTypes.length) + 1;
             System.out.println("INSERT INTO Assignments ( assignment_name, assignment_id, type_id ) VALUES ( '" + assignmentName + "', " + (i + 1) + ", " + assignmentTypeInt + ");");
         }
-        return assignments;
     }
 
+    // TODO: Make sure that the index of a teacher is the id of the teacher -1
     public static void generateTeachers(int n) {
         ArrayList<String> teachers = getFileData("src/teachernames.csv");
         String[] teacherNames = teachers.get(0).split(",");
@@ -172,29 +175,41 @@ public class Main {
         }
     }
 
-//    public static void generateClasses() {
-//        int class_id = 1;
-//        ArrayList<String> periods_to_rooms_to_courses = new ArrayList<>();
-//        for (String classroom : classrooms) {
-//            for (int period = 1; period <= 10; period++) {
-//                periods_to_rooms_to_courses.add(period + "," + classroom);
-//            }
-//        }
-//
-//        for (int i = 0; i < courses.size(); i++) {
-//            periods_to_rooms_to_courses.set(i, periods_to_rooms_to_courses.get(i) + "," + courses.get(i));
-//        }
-//
-//        for (int teacher = 1; teacher <= teachers.getFirst().split(",").length; teacher++) {
-//            for (int period  = 0; period < 10; period++) {
-//                if (class_id <= courses.size()) {
-//                    String[] course = periods_to_rooms_to_courses.get(class_id - 1).split(",");
-//                    System.out.println("INSERT INTO Classes ( class_id, course_id, class_period, teacher_id, classroom_id ) VALUES ( " + class_id + ", " + course[2] + ", " + period + ", " + teacher + ", '" + course[1] + "' );");
-//                    class_id++;
-//                }
-//            }
-//        }
-//    }
+    public static void generateClasses() {
+        int class_id = 1;
+        ArrayList<String> periods_to_rooms_to_courses = new ArrayList<>();
+        for (String classroom : classrooms) {
+            for (int period = 1; period <= 10; period++) {
+                periods_to_rooms_to_courses.add(period + "," + classroom);
+            }
+        }
+
+        for (int i = 0; i < courses.size(); i++) {
+            periods_to_rooms_to_courses.set(i, periods_to_rooms_to_courses.get(i) + "," + courses.get(i).split(",")[1]);
+            System.out.println(periods_to_rooms_to_courses.get(i));
+        }
+        periods_to_rooms_to_courses = new ArrayList<>(Arrays.asList(periods_to_rooms_to_courses.stream().filter(a -> a.split(",").length == 3).toArray(String[]::new)));
+
+        for (int teacher = 1; teacher <= teachers.getFirst().split(",").length; teacher++) {
+            for (int period  = 0; period < 10; period++) {
+                if (!periods_to_rooms_to_courses.isEmpty()) {
+                    int random = (int) (Math.random() * periods_to_rooms_to_courses.size() - 1);
+                    String[] course = periods_to_rooms_to_courses.get(random).split(",");
+                    int course_id = -1;
+
+                    for (int i = 0; i < courses.size(); i++) {
+                        if (courses.get(i).split(",")[1].equals(periods_to_rooms_to_courses.get(random).split(",")[2])) {
+                            course_id = i + 1;
+                        }
+                    }
+                    periods_to_rooms_to_courses.remove(random);
+                    System.out.println(courses);
+                    System.out.println("INSERT INTO Classes ( class_id, course_id, class_period, teacher_id, classroom_id ) VALUES ( " + class_id + ", " + course_id + ", " + period + ", " + teacher + ", '" + course[1] + "' );");
+                    class_id++;
+                }
+            }
+        }
+    }
 
     public static ArrayList<String> getFileData(String fileName) {
         ArrayList<String> fileData = new ArrayList<String>();
